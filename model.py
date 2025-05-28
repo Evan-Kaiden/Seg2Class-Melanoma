d3cd4da2f9875917f5c87d185f12bf0d302ba997
Evan Kaiden <evankaiden@stevenson-100-64-40-41.resnet.ucsc.edu>
2025-05-27 23:20:37 -0700

    finished model and created custom dataloader

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 
@@ -12,15 +13,19 @@ class down(nn.Module):
             self.conv = nn.Sequential(
                 nn.Conv2d(in_channels, 2 * in_channels, kernel_size=3, padding=1),
                 nn.ReLU(),
+                nn.Dropout2d(p=0.3),
                 nn.Conv2d(2 * in_channels, 2 * in_channels, kernel_size=3, padding=1),
-                nn.ReLU(),   
+                nn.ReLU(),  
+                nn.Dropout2d(p=0.3), 
             )
         else:
             self.conv = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
                 nn.ReLU(),
+                nn.Dropout2d(p=0.3),
                 nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
                 nn.ReLU(),   
+                nn.Dropout2d(p=0.3),
             )
         self.max_pool = nn.MaxPool2d(kernel_size=(2,2), stride=2)
 
@@ -40,14 +45,18 @@ class up(nn.Module):
         self.conv_cat = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(),
+            nn.Dropout2d(p=0.3),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(),
+            nn.Dropout2d(p=0.3),
         )
         self.conv_none = nn.Sequential(
             nn.Conv2d(in_channels // 2, out_channels, kernel_size=3, padding=1),
             nn.ReLU(),
+            nn.Dropout2d(p=0.3),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(),
+            nn.Dropout2d(p=0.3),
         )
 
     def forward(self, x, skip_connection=None):
@@ -92,23 +101,57 @@ class Unet(nn.Module):
         u3 = self.up3(u2, None)
         out = self.out(u3)
 
-        return out
+
+        # return mask as well as residual outputs
+        return out, d0, d1, d2, d3
     
 
-def test_unet_forward():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    model = Unet(in_channels=3, n_classes=2).to(device)
-    
-    # Create a random input tensor with batch size 1, 3 channels, 256x256
-    input_tensor = torch.randn(1, 3, 256, 256).to(device)
-    
-    # Forward pass
-    output = model(input_tensor)
+class Seg2Class(nn.Module):
+    def __init__(self, in_channels, n_classes):
+        super(Seg2Class, self).__init__()
 
-    print("Output shape:", output.shape)
+        self.mlp = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),       # (B, C, 1, 1)
+            nn.Flatten(),                  # (B, C)
+            nn.Linear(128 + 1, n_classes), # (B, n_classes)
+            nn.Dropout(p=0.3)
+        )
 
-    # Check output shape (should match batch size and be [1, n_classes, H, W])
-    assert output.shape == (1, 2, 256, 256), f"Unexpected output shape: {output.shape}"
-    print("Unet forward pass successful.")
+        self.unet = Unet(in_channels, n_classes=1)
 
+    def forward(self, x):
+        mask, d0, d1, d2, d3 = self.unet(x)
+        
+        # downsample the mask 
+        B, C, H, W = d1.shape
+        scaled_mask = F.interpolate(mask, size=(H, W), mode='bilinear', align_corners=False)
+
+        # Concat
+        d1_mask = torch.concat([d1, scaled_mask], dim = 1) # (B x C+1 x H x W)
+
+        # Predict
+        classification = self.mlp(d1_mask)
+
+        return classification, mask
+
+
+
+        
+
+        
+
+
+
+x = torch.randn(1, 3, 256, 256) 
+
+model = Seg2Class(in_channels=3, n_classes=10)
+
+model.eval()
+
+with torch.no_grad():
+    out, mask = model(x)
+
+# Print the shapes
+print(f"Input shape:      {x.shape}")
+print(f"Output Shape:     {out.shape}")
+print(f"Mask shape:         {mask.shape}")
---
+++ seg_dataloader.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+import os
+import cv2
+import pandas as pd
+
+class segmentationLoader(Dataset):
+    def __init__(self, image_paths, csv_path):
+        self.image_paths = image_paths
+        self.csv = pd.read_csv(csv_path)
+
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        
+        x = cv2.imread(image_path)
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+
+        base_name = os.path.splitext(os.path.basename(image_path))[0]
+        seg_path = image_path.replace("Data", "GroundTruth").replace(base_name, base_name + "_Segmentation")
+        seg_path = os.path.splitext(seg_path)[0] + ".png"
+
+        seg = cv2.imread(seg_path)
+        seg = cv2.cvtColor(seg, cv2.COLOR_BGR2RGB)
+
+        image_name = image_path.split('/')[-1]
+        image_name = image_name.split('.')[0]
+        label_row = self.csv[self.image == image_name]
+        label = self.csv[label_row].label
+
+        if isinstance(label, str):
+            if label == "benign":
+                label = 0
+            else:
+                label = 1
+
+
+        x = torch.tensor(x.transpose(2, 0, 1), dtype=torch.float32) / 255.0
+        seg = torch.tensor(seg.transpose(2, 0, 1), dtype=torch.float32) / 255.0
+        label = torch.tensor(label, dtype=torch.float32)
+
+        return x, seg, label
